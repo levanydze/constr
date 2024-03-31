0ccd30bd5cd598a50ae4dafa99b66ae2ac0df304
levanydze <66974840+levanydze@users.noreply.github.com>
2024-03-31 17:44:18 +0200

    1

--- src/app/chacha/page.tsx
+++ src/app/chacha/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { get, ref, getDatabase } from "firebase/database";
-import { app } from "../firebaseConfig";
+import { auth as app } from "../firebaseConfig";
 import ChachaCard from "./ChachaCard";
 import PageHeadImage from "../../../components/sectionComponents/PageHeadImage/PageHeadImage";
 import imagesJson from "../../../json/images.json";
--- src/app/firebaseConfig.js
+++ src/app/firebaseConfig.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+// export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const database = getDatabase(app);
 export { database };
